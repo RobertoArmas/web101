88d26e00495469525df586e9de21f8d84a6813d1
Roberto Armas <robertoarmas95@hotmail.com>
2018-09-17 10:55:54 -0500

    edit y delete

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -66,7 +66,7 @@ class MessageController extends Controller
      */
     public function edit(Message $message)
     {
-        //
+        return view('messages.edit')->with('message',$message);
     }
 
     /**
@@ -78,7 +78,13 @@ class MessageController extends Controller
      */
     public function update(Request $request, Message $message)
     {
-        //
+        $message->text = $request->get('texto');
+         $success = $message->save();
+        if($success){
+            return redirect(route('messages.index'));
+        }else{
+            return redirect()->back()->with('error',"no se pudo actualizar");
+        }
     }
 
     /**
@@ -89,6 +95,11 @@ class MessageController extends Controller
      */
     public function destroy(Message $message)
     {
-        //
+        $success = $message->delete();
+        if($success){
+            return redirect()->back()->with('success',"Se ha eliminado exitosamente!");
+        }else{
+            return redirect()->back()->with('error',"no se pudo ingresar");
+        }
     }
 }
