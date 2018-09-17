fc41e9178e50a0f15f257c17e916c8a5bec81703
KevinSoto1 <39774203+KevinSoto1@users.noreply.github.com>
2018-09-17 10:57:54 -0500

    CRUD completo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     /**
      * Show the application dashboard.
      *
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('/messages',MessageController::class);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
